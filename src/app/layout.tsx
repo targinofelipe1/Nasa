657f0381c9bb3a57fe5186ac35413533a7b79b00
jardsousa <aaaedenn47@gmail.com>
2025-03-13 10:21:45 -0300

    modificação da rota página principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import MapsPage from "./page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +28,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        
       >
-        <Toaster richColors/>
+        <Toaster richColors />
         {children}
       </body>
     </html>
