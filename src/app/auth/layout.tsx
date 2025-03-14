642f695d6abbd73c450a1a0b17f882a71aded8bb
targino <targino.felipe@academico.ifpb.edu.br>
2025-03-14 00:25:56 -0300

    centralização de título

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout({
       {/* Coluna Esquerda - Formulário */}
       <div className="flex flex-col gap-4 p-6 md:p-10">
         {/* Logo e Nome */}
-        <div className="flex justify-center lg:justify-start items-center gap-2">
+        <div className="flex justify-center items-center gap-2 text-center">
           <a href="/auth/sign-in" className="flex items-center gap-2 font-medium">
             <div className="flex h-7 w-7 items-center justify-center rounded-md bg-emerald-500 text-primary-foreground">
               <BarChart2 className="size-6" />
@@ -18,6 +18,7 @@ export default function AuthLayout({
             Secretaria de Estado do Desenvolvimento Humano
           </a>
         </div>
+
 
 
         {/* Conteúdo Dinâmico (Login ou Cadastro) */}
