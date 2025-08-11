690c70280a7dbabe820693540637fb31fd5398a2
Arthur Felipe <arthurfelipe5567@gmail.com>
2025-08-11 10:05:25 -0300

    feat: add partial requests to redis
    so it doesnt brake the per request limit

--- src/app/api/sheets/eleicao/[id]/route.ts
+++ src/app/api/sheets/eleicao/[id]/route.ts
@@ -6,12 +6,15 @@ import { NextRequest, NextResponse } from 'next/server'
 import { PLANILHAS } from '@/services/sheetService'
 import { getBaseUrl } from '@/utils/url'
 
+
 export async function GET(
   req: NextRequest,
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params
   const spreadsheetId = PLANILHAS[id]
+  const MAX_BYTES = 900_000
+  const BATCH_SIZE = 5
 
   if (!spreadsheetId) {
     return NextResponse.json(
@@ -21,19 +24,29 @@ export async function GET(
   }
 
   try {
-    const cache = await cacheDb.get(id)
     const ttl = await cacheDb.ttl(id)
     const baseUrl = getBaseUrl(req)
 
-    if (!cache) {
+    if (ttl == -2) {
       const data = await getSheetDataByRange(spreadsheetId, id)
+      const json = JSON.stringify(data)
+      const buffer = Buffer.from(json, 'utf-8')
 
-      await cacheDb.set(id, JSON.stringify(data), { ex: 60 * 60 * 24 * 30 })
-      
-      // await jobs.publish({
-      //   url: new URL(`/cache/votacao/${id}`, baseUrl).toString(),
-      //   body: JSON.stringify(data)
-      // })
+      let chunks = []
+
+      for (let i = 0; i < buffer.length; i += MAX_BYTES) {
+        const chunk = buffer.subarray(i, i + MAX_BYTES)
+        chunks.push(chunk.toString('utf-8'))
+
+        if (chunks.length >= BATCH_SIZE) {
+          await cacheDb.rpush(id, ...chunks)
+          chunks = []
+        }
+      }
+
+      await cacheDb.rpush(id, ...chunks)
+      await cacheDb.expire(id, ((60*60)*24)*30)
+
 
       return NextResponse.json({ success: true, data })
     }
@@ -43,6 +56,14 @@ export async function GET(
         url: new URL(`/cache/votacao/${id}`, baseUrl).toString()
       })
     }
+
+    const listLen = await cacheDb.llen(id)
+    let jsonString = ''
+    for (let i = 0; i < listLen; i += BATCH_SIZE) {
+      jsonString += (await cacheDb.lrange(id, i, i + (BATCH_SIZE - 1))).join('')
+    }
+
+    const cache = JSON.parse(jsonString)
 
     return NextResponse.json({ success: true, data: cache })
   } catch (error: any) {
